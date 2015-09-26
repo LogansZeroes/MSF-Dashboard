7c09f03139ef46442582e609c013cbb140418646
Jimin Sung <jimin@Jimins-MacBook-Pro.local>
2015-09-26 19:05:29 -0400

    mongo garbage collection

--- server/app/routes/data/index.js
+++ server/app/routes/data/index.js
@@ -8,6 +8,11 @@ var dweetio = new dweetClient();
 var Dweet = require('mongoose').model('Dweet');
 
 router.get('/latest', function (req, res) {
+    var currDate = new Date();
+    var currTime = currDate.getTime();
+    var hourBack = currTime - 1000*3600;
+    var minBack = currTime - 1000*60;
+
     dweetio.get_latest_dweet_for("calm-patch", function(err, latestDweet){
 
         var dweet = latestDweet[0]; // Dweet is always an array of 1
@@ -19,6 +24,9 @@ router.get('/latest', function (req, res) {
         };
 
         Dweet.create(obj);
+
+        //Remove all dweets prior to the last hour (for garbage collection)
+        Dweet.find({created: {$lte: minBack} }).remove().exec();
         // console.log('The Name ', dweet.thing); // The generated name
         // console.log('The Content ', dweet.content); // The content of the dweet
         // console.log(dweet.created); // The create date of the dweet
